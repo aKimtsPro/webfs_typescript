89dcb83c1faef55d0af84cb969d215fa93f7bdf5
akimt <a.kimtsaris@outlook.com>
2020-11-19 16:03:31 +0100

    reorganisation

---
+++ dist/4_interface.js
@@ -0,0 +1,19 @@
+// a partir d'une personne on peut acceder à tous ses proprietes
+function sePresenter(p) {
+    console.log("bonjour, je m'appelle " + p.nom + " et j'ai " + p.age + " ans. NUM:" + p.num_nat);
+    // p.nom = "marie"; // erreur : readonly
+    if (p.manger != undefined)
+        p.manger("burger");
+}
+var vFunc3 = function (x) { console.log("scrounch"); };
+var obj = {
+    nom: "luc",
+    age: 15,
+    num_nat: 0,
+    manger: vFunc3 // pas de declaration de function dans la declaration d'objet // optionnel
+};
+var pers = obj;
+sePresenter(pers);
+// l'objet en tant qu'Object continue à ne pas être en readonly
+obj["nom"] = "marie";
+sePresenter(pers);
